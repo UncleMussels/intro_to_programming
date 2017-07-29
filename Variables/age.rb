11f7d618a9794560aa7596b0d0282dcfa4f567ae
Owen Turkle <owen@turkle.net>
2017-07-28 19:23:20 -0700

    Added age.rb

---
+++ Variables/age.rb
@@ -0,0 +1,7 @@
+puts "What is your current age, in years?"
+age = gets.chomp.to_i
+
+puts "You will be #{age+ 10} in ten years, " + 
+     "#{age + 20} in twenty years, " + 
+     "#{age + 30} in thirty years, " + 
+     "and #{age + 40} in forty years!"
