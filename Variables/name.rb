c36fcdab50b3345e54c798368364575365e4bdcb
Owen Turkle <owen@turkle.net>
2017-07-28 19:28:34 -0700

    Updated name.rb w/ exercise 3

--- Variables/name.rb
+++ Variables/name.rb
@@ -1,4 +1,11 @@
+# Exercise 1
+
 puts "Give us your name!"
 name = gets.chomp
 
-puts "#{name}, you are most obedient."
+puts "#{name}, you are most obedient. I shall now repeat your name 10 times in prayer!"
+
+
+# Exercise 3
+
+10.times { |x| puts name}
