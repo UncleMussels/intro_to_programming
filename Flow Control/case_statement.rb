7dcb646670d0ca2a9c4edd35a92936e1afad527c
Owen Turkle <owen@turkle.net>
2017-07-29 17:11:40 -0700

    Added in-chapter exercises for conditionals and case statements

---
+++ Flow Control/case_statement.rb
@@ -0,0 +1,14 @@
+# case statements, refactored
+
+a = 10
+
+answer = case 
+  when a == 10
+    puts "a is 10"
+  when a == 11
+    puts "a is 11"
+  else
+    puts "a is not 5 and a is definitely not six"
+  end
+
+puts answer
