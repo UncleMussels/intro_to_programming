c70c66189247392e04f4c73a3a7742ea9f156bdc
Owen Turkle <owen@turkle.net>
2017-07-29 17:58:44 -0700

    Updated number_comparison with exercise 5

--- Flow Control/number_comparison.rb
+++ Flow Control/number_comparison.rb
@@ -1,14 +1,19 @@
-# exercise 3: comparing numbers
+# exercise 3 + 5: comparing numbers
 
+def case_number(num)
+  case 
+  when num < 0
+    puts "WRONG"
+  when num <= 50
+    puts "Your number is between 0 and 50."
+  when num <= 100
+    puts "Your number is between 51 and 100."
+  else
+    puts "Your number is over 9000!"
+  end
+end
 
 puts "Give us a number between 0 and 100, poppet:"
 answer = gets.chomp.to_i
 
-case 
-when answer <= 50
-  puts "Your number is between 0 and 50."
-when answer <= 100
-  puts "Your number is between 51 and 100."
-else
-  puts "Your number is over 9000!"
-end
+case_number(answer)
