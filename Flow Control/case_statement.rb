7652bd9ccadbbba4a423579d2526814930356854
Owen Turkle <owen@turkle.net>
2017-08-10 12:03:44 -0600

    Simplified case_statement.rb

--- Flow Control/case_statement.rb
+++ Flow Control/case_statement.rb
@@ -4,11 +4,11 @@ a = 10
 
 answer = case 
   when a == 10
-    puts "a is 10"
+    "a is 10"
   when a == 11
-    puts "a is 11"
+    "a is 11"
   else
-    puts "a is not 5 and a is definitely not six"
+    "a is not 5 and a is definitely not six"
   end
 
 puts answer
