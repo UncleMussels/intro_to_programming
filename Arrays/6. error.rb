1d04b484872a9a38ca50049c9f2cc071a5744c02
Owen Turkle <owen@turkle.net>
2017-08-10 22:56:19 -0600

    Added exercise 6

---
+++ Arrays/6. error.rb
@@ -0,0 +1,11 @@
+#decipher the error message
+
+names = ['bob', 'joe', 'susan', 'margaret']
+names['margaret'] = 'jody'
+
+#TypeError: no implicit conversion of String into Integer
+  #from (irb):2:in `[]='
+  #from (irb):2
+  #from /Users/username/.rvm/rubies/ruby-2.0.0-p353/bin/irb:12:in `<main>'
+
+#problem/solution: used value 'margaret' instead of index number [3]
