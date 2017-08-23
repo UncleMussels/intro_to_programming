a00d9698927b77261d67c8a89e7cad9645eb5c07
Owen Turkle <owen@turkle.net>
2017-08-23 16:27:49 -0700

    Updated regex examples

--- Other Topics/regex.rb
+++ Other Topics/regex.rb
@@ -11,3 +11,20 @@ end
 regex_hunter("imbecile")
 regex_hunter("Brooklyn")
 regex_hunter("ballboy")
+
+puts "\n"
+
+# boolean_matchdata.rb
+
+def has_a_b?(string)
+  if /b/.match(string)
+    puts "We have a match!"
+  else
+    puts "No match here."
+  end
+end
+
+has_a_b?("basketball")
+has_a_b?("football")
+has_a_b?("hockey")
+has_a_b?("golf")
