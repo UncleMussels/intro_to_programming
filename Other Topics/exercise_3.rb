8ab1ca0b106e81653976f1266bbe9b385c469c8a
Owen Turkle <owen@turkle.net>
2017-08-29 10:59:47 -0700

    Added exercise 3

---
+++ Other Topics/exercise_3.rb
@@ -0,0 +1,3 @@
+# Question: What is exception handling and what problem does it solve?
+
+#Answer: exception handling a process to deal with errors in a manageable and predictable way. It stays the course of the program when it encounters exceptional conditions
